1806ddb24c2255885ff0468b24652c8ff767c03c
kendi ardi <arsamisapper12@gmail.com>
2018-04-10 10:40:05 +0800

    Instance,buat atribut dan panggil method

--- src/praktek03/SepatuAksi.java
+++ src/praktek03/SepatuAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class SepatuAksi {
     public static void main(String[] args) {
-        
+       Sepatu a = new Sepatu();
+       a.merk="Yeezy";
+       a.warna="hitam";
+       a.harga=500.000;
+       
+       a.cetakInfo();
     }     
 }
