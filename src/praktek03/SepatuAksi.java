d001a986de89df90921a166f90c24f695dd9c7e9
kendi ardi <arsamisapper12@gmail.com>
2018-04-10 10:52:33 +0800

    menggunakan method set

--- src/praktek03/SepatuAksi.java
+++ src/praktek03/SepatuAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class SepatuAksi {
     public static void main(String[] args) {
        Sepatu a = new Sepatu();
-       a.merk="Yeezy";
-       a.warna="hitam";
-       a.harga=500.000;
+
+       a.setMerk("Yeezy");
+       a.setWarna("Hitam");
+       a.setHarga(500.000);
        
        a.cetakInfo();
     }     
